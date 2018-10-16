3acd7068acda7621c56ec77e02385003cfd5b2c4
rchu9700 <rchu9700@bths.edu>
2018-10-16 14:44:26 -0400

    circle done

---
+++ src/Circle.java
@@ -0,0 +1,16 @@
+public class Circle extends Shape {
+    private int radius;
+
+    public Circle (int radius) {
+        super( "Circle");
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius,2);
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
