4e13c997d5cedba47398a3ad3b37e209f6933e1f
rchu9700 <rchu9700@bths.edu>
2018-10-17 14:40:41 -0400

    done

--- src/ETriangle.java
+++ src/ETriangle.java
@@ -15,15 +15,4 @@ public class ETriangle extends Shape {
     }
 }
 
-    public static void findArea(int sideLength)
-    {
-        int x= (int) (Math.sqrt(3)) / 4;
-        int area= (sideLength * sideLength) * x;
-        System.out.println("The area of an equilateral triangle with side length " + sideLength + " is" + area + ".");
-    }
 
-    public static void findPerimeter(int sideLength)
-    {
-        int perimeter= 3 * sideLength;
-        System.out.println("The perimeter of an equilateral triangle with side length " + sideLength + "is " + perimeter + ".");
-    }
